91259082a6bb46988a8e584e941c33f59c8b1c99
Lomgle <jmmj1293@gmail.com>
2026-07-22 01:37:39 +0700

    irrelevant

---
+++ Assets/Scripts/snakeMovement.cs
@@ -0,0 +1,23 @@
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class snakeMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector2 direction;
+    void controlDirection()
+    {
+        ;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
