2e5367b23137ae295b58c65d3168cc750478d014
Lomgle <jmmj1293@gmail.com>
2026-07-25 01:49:44 +0700

    undone

--- Assets/Scripts/snakeMovement.cs
+++ Assets/Scripts/snakeMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +7,15 @@ public class SnakeMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Vector2 direction;
+    private List<Transform> segmentList;
+    public Transform segmentPrefab;
+    private bool hasntEaten = true;
+    public Vector3 previousPosition;
+    void Start()
+    {
+        segmentList = new List<Transform>();
+        segmentList.Add(transform);
+    }
     public void ControlDirection()
     {
         if (Keyboard.current.aKey.isPressed) direction = Vector2.left;
@@ -14,12 +25,32 @@ public class SnakeMovement : MonoBehaviour
     }
     void Update()
     {
+        for (int i = segmentList.Count - 1; i > 0; i--)
+        {
+            segmentList[i].position = segmentList[i - 1].position;
+        }
         ControlDirection();
     }
 
     // Update is called once per frame
     public void FixedUpdate()
     {
-        this.transform.position = new Vector3(this.transform.position.x + direction.x, this.transform.position.y + direction.y, 0.0f);
+        if (hasntEaten) previousPosition = transform.position;
+        transform.position = new Vector3(transform.position.x + direction.x, transform.position.y + direction.y, 0.0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = segmentList[^1].position; //^1 = -1
+        segmentList.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Food")
+        {
+            Grow();
+        }
     }
 }
