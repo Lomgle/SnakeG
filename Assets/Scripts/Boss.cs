923351e95977d8c21d5b53e71f541492468c2c5f
Lomgle <jmmj1293@gmail.com>
2026-08-03 14:03:42 +0700

    upd

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -10,8 +10,14 @@ public class Boss : MonoBehaviour
     public GameObject food;
     public GameObject boss_wall1;
     public LayerMask bossLayer;
+    public BoxCollider2D bombGrid1;
+    private Bounds bombGrid1Bounds;
+
+    public GameObject bomb_indicator;
+    public GameObject bomb;
     void Start()
     {
+        bombGrid1Bounds = bombGrid1.bounds;
         snake = GameObject.FindGameObjectWithTag("Player").GetComponent<SnakeMovement>();
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<Logic>();
     }
@@ -37,13 +43,45 @@ public class Boss : MonoBehaviour
         playMusic();
         bossAnim.SetTrigger("Intro");
         yield return new WaitForSeconds(introTime);
+
+        snake.direction = Vector2.right;
         snake.canMove = true;
         boss_wall1.SetActive(true);
+        StartCoroutine(BombScatter());
+    }
 
-        
+    IEnumerator BombScatter()
+    {
+        float explodeTime = 2f;
+
+        StartCoroutine(SpawnBomb(bombGrid1Bounds, bossLayer, explodeTime));
+
+        yield return new WaitForSeconds(4f);
+
+        for (int i = 1; i <= 3; i++){
+            StartCoroutine(SpawnBomb(bombGrid1Bounds, bossLayer, explodeTime));
+        }
+
+        yield return new WaitForSeconds(3f);
+        for (int i = 1; i <= 5; i++)
+        {
+            StartCoroutine(SpawnBomb(bombGrid1Bounds, bossLayer, explodeTime));
+        }
+
+        yield return new WaitForSeconds(3f);
+        for (int i = 1; i <= 5; i++)
+        {
+            StartCoroutine(SpawnBomb(bombGrid1Bounds, bossLayer, explodeTime/2));
+        }
+
+        yield return new WaitForSeconds(2f);
+        for (int i = 1; i <= 5; i++)
+        {
+            StartCoroutine(SpawnBomb(bombGrid1Bounds, bossLayer, explodeTime));
+        }
     }
     
-    public void RandomizePos(Bounds bounds, LayerMask layer)
+    public Vector2 RandomizePos(Bounds bounds, LayerMask layer)
     {
         float x, y;
         Vector2 temp;
@@ -54,5 +92,20 @@ public class Boss : MonoBehaviour
             temp.x = x;
             temp.y = y;
         } while (Physics2D.OverlapPoint(temp, layer));
+        return temp;
+    }
+
+    IEnumerator SpawnBomb(Bounds bounds, LayerMask layer, float explodeTime)
+    {
+        Vector2 bomb_pos = RandomizePos(bounds, layer);
+
+        GameObject bomb_indicator1 = Instantiate(bomb_indicator);
+        bomb_indicator1.transform.position = bomb_pos;
+
+        yield return new WaitForSeconds(explodeTime);
+
+        GameObject bomb1 = Instantiate(bomb);
+        bomb1.transform.position = bomb_pos;
+
     }
 }
